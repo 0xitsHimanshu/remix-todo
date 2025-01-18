55fd2bf3e0d5ff8e74e2c5f38122badd702e6b85
Himanshu <70974599+0xitsHimanshu@users.noreply.github.com>
2025-01-18 17:56:55 +0530

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 // import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
@@ -9,17 +10,108 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const tasks: string[] = [];
+  
   return (
-    <div className="flex h-screen items-center justify-center">
-      <header className="flex justify-between w-full p-4">
-        <h1>Todo</h1>
-        <select>
+    <div className="flex flex-1 items-center h-screen flex-col">
+      <header className="mb-12 pt-6 px-6 flex items-center justify-between w-full">
+        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">Todo</h1>
+        <select className="appearance-none rounded-3xl border border-gray-200 bg-gray-50 px-4 py-2 text-center ">
           <option>System</option>
           <option>Light</option>
           <option>Dark</option>
         </select>
       </header>
-      <h1 className="text-2xl font-bold">Hello, World!</h1>
+      <main className="flex-1 space-y-8">
+        <form className="rounded-full border border-gray-200 bg-white/90 shadow-md ">
+          <fieldset className="flex items-center gap-2 p-2 text-sm">
+            <input
+              type="text"
+              name="description"
+              placeholder="Create a new todo..."
+              required
+              className="flex-1 rounded-full border-2 border-gray-200 px-3 py-2 text-sm font-bold text-black dark:border-white/50"
+            />
+            <button className="rounded-full border-2 border-gray-200/50  px-3 py-2 text-base font-black transition hover:scale-105 hover:border-gray-500 sm:px-6 ">
+              Add
+            </button>
+          </fieldset>
+        </form>
+ 
+        <div className="rounded-3xl border border-gray-200 bg-white/90 px-4 py-2">
+          {tasks.length > 0 ? (
+            <ul>
+              {tasks.map((task) => (
+                <li key={task}>{task}</li>
+              ))}
+            </ul>
+          ) : (
+            <p className="text-center leading-7">No tasks available</p>
+          )}
+        </div>
+ 
+        <div className="rounded-3xl border border-gray-200 bg-white/90 px-4 py-2">
+          <div className="flex items-center justify-between gap-4 text-sm">
+            <p className="text-center leading-7">
+              {tasks.length} {tasks.length === 1 ? "item" : "items"} left
+            </p>
+            <div className="flex items-center gap-4">
+              <button className="text-red-400 transition hover:text-red-600">
+                Clear Completed
+              </button>
+              <button className="text-red-400 transition hover:text-red-600">
+                Delete All
+              </button>
+            </div>
+          </div>
+        </div>
+ 
+        <div className="rounded-3xl border border-gray-200 bg-white/90 px-4 py-2">
+          <div className="flex items-center justify-center gap-12 text-sm">
+            <button
+              aria-label="View all tasks"
+              className="opacity-50 transition hover:opacity-100"
+            >
+              All
+            </button>
+            <button
+              aria-label="View active tasks"
+              className="opacity-50 transition hover:opacity-100"
+            >
+              Active
+            </button>
+            <button
+              aria-label="View completed"
+              className="opacity-50 transition hover:opacity-100"
+            >
+              Completed
+            </button>
+          </div>
+        </div>
+      </main>
+
+      <footer className="mt-12 px-6">
+        <p className="text-sm text-center leading-loose">
+          Built by {" "}
+          <Link
+            to={'https://github.com/0xitsHimanshu'}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="relative font-medium text-white after:absolute after:-bottom-0.5 after:left-0 after:h-[1px] after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full dark:text-blue-500 dark:after:bg-blue-500"
+          >
+            Himanshu
+          </Link>
+          {" "} with ❤️. The source code is available on {" "}
+          <Link
+            to={'https://github.com/0xitsHimanshu/remix-todo'}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="relative font-medium text-white after:absolute after:-bottom-0.5 after:left-0 after:h-[1px] after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full dark:text-blue-500 dark:after:bg-blue-500"
+          >
+            Github
+          </Link>
+        </p>
+      </footer>
     </div>
   );
 }
